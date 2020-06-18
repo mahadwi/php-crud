71d734769c1944716ad03a4fe5775d657562ed41
mahadwigit config --global user.email mahadwiputra008@gmail.com <mahadwiputra008@gmail.com>
2020-06-18 10:34:18 +0700

    menambahkan folder koneksi dan file koneksi.php

---
+++ koneksi/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "test";
+
+//koneksi database
+$koneksi = mysqli_connect($server, $username, $password, $database) or die;
